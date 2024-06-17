ccd565b4d20ca4ff722c1c2f79e25dcb21569c75
Justine Fricou <justine.fricou@gmail.com>
2024-06-17 12:17:25 +0200

    Add writing in file from cypress test

--- cypress/integration/nav_create_trek.js
+++ cypress/integration/nav_create_trek.js
@@ -22,12 +22,15 @@ describe('Create trek', () => {
     });
 
     it('Create trek', () => {
+        cy.writeFile('benchmark_js.txt', 'test 1 ', { flag: 'a+' })
         cy.visit('/trek/list');
         cy.wait('@tiles');
         cy.server();
         cy.route('/api/path/drf/paths/graph.json').as('graph');
         cy.get("a.btn-success[href='/trek/add/']").contains('Add a new trek').click();
         cy.wait('@graph');
+        cy.writeFile('benchmark_js.txt', 'test 2', { flag: 'a+' })
+        cy.writeFile('benchmark_js.txt', '\n', { flag: 'a+' })
         cy.get("a.linetopology-control").click();
         cy.get("textarea[id='id_topology']").type('[{"pk": 2, "kind": "TREK", "offset": 0.0, "paths": [3], "positions": {"0": [0.674882030756843, 0.110030805790642]}}]', {
             force: true,
