fa5bc760c011c6982a2d4267547b32370b756f29
Justine Fricou <justine.fricou@gmail.com>
2024-06-18 15:06:48 +0200

    Modify the base cypress benchmark routing script so it executes without error

--- benchmarking/cypress/e2e/benchmark_automation.cy.js
+++ benchmarking/cypress/e2e/benchmark_automation.cy.js
@@ -24,22 +24,22 @@ describe('Frontend-side routing', () => {
 
     it('Find a route', () => {
 
-        cy.visit('/trek/list');
+        cy.visit('/trek/add');
         cy.wait('@tiles');
-        cy.server();
 
         // Get the graph and measure the execution time
-        cy.route('/api/path/drf/paths/graph.json').as('graph');
         let startTime = 0
         cy.then(() => startTime = performance.now());
-        cy.wait('@graph')
+        cy.intercept('/api/path/drf/paths/graph.json')
         .then(() => {
             let elapsedTime = performance.now() - startTime
             cy.writeFile('benchmark_js.txt', elapsedTime.toString() + ' ', { flag: 'a+' })
             startTime = performance.now()
         });
 
+        // Click on the "Route" control
         cy.get("a.linetopology-control").click();
+
         cy.get("textarea[id='id_topology']").type('[{"pk": 2, "kind": "TREK", "offset": 0.0, "paths": [3], "positions": {"0": [0.674882030756843, 0.110030805790642]}}]', {
             force: true,
             parseSpecialCharSequences: false
