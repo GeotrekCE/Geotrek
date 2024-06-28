15fa90606511819a6d4e90c75c6965631ceb82a6
Justine Fricou <justine.fricou@gmail.com>
2024-06-28 16:07:08 +0200

    Setting bigger timeouts for cypress

--- benchmarking/cypress.config.js
+++ benchmarking/cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     baseUrl: 'http://geotrek.local:8000',
   },
   video: false,
-  requestTimeout: 50000,
+  defaultcommandTimeout: 5000000,
+  requestTimeout: 5000000,
+  responseTimeout: 5000000,
   projectId: "ktpy7v"
 });
