6324231110cb5fb58d22f9f3efd225905ba68e96
Justine Fricou <justine.fricou@gmail.com>
2024-07-01 13:48:14 +0200

    Remove useless headers in curl cache removing

--- benchmarking/benchmark.sh
+++ benchmarking/benchmark.sh
@@ -14,7 +14,7 @@ fi
 # Launch the cypress tests to take the time measures
 for i in $(seq 1 $NB_MEASURES)
 do
-    curl 'http://geotrek.local:8000/admin/clearcache/' -X POST -H 'Origin: http://geotrek.local:8000' -H 'Connection: keep-alive' -H 'Referer: http://geotrek.local:8000/admin/clearcache/' -H 'Cookie: csrftoken=jJPzy1w4p7KNspD9QG1Y2xOqG8Oczf2l; sessionid=xm0ia4g7gpjdtq9l85sbff3gscdlobfl; django_language=en' -H 'Upgrade-Insecure-Requests: 1' -H 'Priority: u=1' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data-raw 'csrfmiddlewaretoken=VihAxtR8JyN10VzyXyyEAUSiwWIbVnPG4RWZVkd2YvnEia2xD4psshwy2UmdksHR&cache_name=fat&clearcache=Clear+cache+now+%F0%9F%92%A3'
+    curl 'http://geotrek.local:8000/admin/clearcache/' -X POST -H 'Cookie: csrftoken=jJPzy1w4p7KNspD9QG1Y2xOqG8Oczf2l; sessionid=xm0ia4g7gpjdtq9l85sbff3gscdlobfl; django_language=en' --data-raw 'csrfmiddlewaretoken=VihAxtR8JyN10VzyXyyEAUSiwWIbVnPG4RWZVkd2YvnEia2xD4psshwy2UmdksHR&cache_name=fat&clearcache=Clear+cache+now+%F0%9F%92%A3'
     npm run cypress:benchmark
 done
 
